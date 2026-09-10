60ffb222df9fbbb6e348e75fedb7a3769d11b690
ADRIKU-app <adian02nava1997@gmail.com>
2026-09-09 23:01:07 -0500

    Agregar funcion graficarRectangulo y reutilizar en graficarGato y graficarComida

--- cazando.js
+++ cazando.js
@@ -11,15 +11,23 @@ let gatoY=canvas.height/2;
 let comidaX=500-ANCHO_COMIDA;
 let comidaY=500-ALTO_COMIDA;
 
+function graficarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle=color;
+    ctx.fillRect(x,y,ancho,alto);
+}
+
 function iniciarJuego(){
     graficarGato();
     graficarComida();
 }
 function graficarGato(){
-    ctx.fillStyle="#F5B027";
-    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+    /*ctx.fillStyle="#F5B027";
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);*/
+    graficarRectangulo(gatoX,gatoY,ANCHO_GATO,ALTO_GATO, "#F5B027");
 }
 
 function graficarComida(){
-    ctx.fillStyle="#BF0A54";
-    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);}
+    /*ctx.fillStyle="#BF0A54";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);*/
+    graficarRectangulo(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA,"#BF0A54");
+}
